3cd61510bf0fd8475b88d9906ebdfb1ce2ceda5b
Mia Starr <starrmiamks@gmail.com>
2021-03-05 20:47:00 -0500

    adding updated code to Mia branch

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -4,49 +4,45 @@ let bcrypt = require('bcryptjs')
 let jwt = require('jsonwebtoken')
 let db = require('../db')
 const User = db.import('../models/user')
-
 //register
-router.post('/register', (req, res) =>{
+router.post('/register', (req, res) => {
     User.create({
-        email: req.body.user.email, 
+        email: req.body.user.email,
         password: bcrypt.hashSync(req.body.user.password, 13)
     })
-    .then((user) => {
-        console.log(user)
-        let token = jwt.sign( {id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24} );
-        res.status(200).json({
-            user: user,
-            message: "user create worked",
-            sessionToken: token
+        .then((user) => {
+            console.log(user)
+            let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+            res.status(200).json({
+                user: user,
+                message: "user create worked",
+                sessionToken: token
+            })
         })
-    })
-    .catch((err) => {
-        console.log("failed to save a user" )
-        res.status(500).json({
+        .catch((err) => {
+            console.log("failed to save a user")
+            res.status(500).json({
                 err: "register did not work"
+            })
         })
-    })
 })
 //login
 router.post('/login', (req, res) => {
     User.findOne({
-        email: req.body.user.email,
-        password: bcrypt.hashSync(req.body.user.password, 13)
+        where: {
+            email: req.body.user.email
+        }
     })
-    .then((user) => {
-        console.log(user)
-        let token = jwt.sign( {id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24} );
-        res.status(200).json({
-            user: user,
-            message: "user login worked",
-            sessionToken: token
-        })
-    })
-    .catch((err) => {
-        console.log("failed to login user" )
-        res.status(500).json({
-                err: "login did not work"
-        })
-    })
-})
-module.exports = router
+        .then(function loginSuccess(user) {
+            if (user) {
+                bcrypt.compare(req.body.user.password, user.password, function (err, matches) {
+                    if (matches) {
+                        let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+                        res.status(200).json({
+                            user: user,
+                            message: "user login worked",
+                            sessionToken: token
+                        })
+                    } else {
+                        res.status(502).send({ err: "Login Failed" });
+                    }
