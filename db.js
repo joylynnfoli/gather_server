a26ff98d91a26ef61673f7bb7abd2756d6702ade
Joylynn Foli <joylynnfoli@gmail.com>
2021-03-06 14:12:54 -0500

    heroku first commit

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("Gather-Server", "postgres", "password", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
 });
 
