cffb09dde0b1e2ef64953efee75b95133b194d32
Joylynn Foli <joylynnfoli@gmail.com>
2021-03-07 13:39:15 -0500

    caching up db file with dev/main

--- db.js
+++ db.js
@@ -1,7 +1,16 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// const sequelize = new Sequelize("Gather-Server", "postgres", "password", {
+//   host: "localhost", 
+
   dialect: "postgres",
-});
+  dialectOptions: {
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+  });
 
 sequelize
   .authenticate()
